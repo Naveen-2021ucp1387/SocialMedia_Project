5a5e44ac70ef262dd92f409d03fa5c6f90998e5c
Naveen chouhan <naveenchouhan2001@gmail.com>
2024-04-06 03:55:29 +0530

    Basic UI setUp

---
+++ src/components/PostList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./Post";
+
+const PostList = (props) => {
+  return (
+    <div>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+    </div>
+  );
+};
+
+export default PostList;
