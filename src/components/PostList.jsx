7fa3305396eae669c04add5b39bf2d1e69d20c66
Naveen chouhan <naveenchouhan2001@gmail.com>
2024-04-09 13:52:14 +0530

    useEffect Hook

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,33 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/Post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 const PostList = (props) => {
   const { postList, addIntialPosts } = useContext(PostListData);
-  const [dataFetched, setDataFetched] = useState(false);
-  if (!dataFetched) {
-    fetch("https://dummyjson.com/posts")
+  const [fetching, setFetching] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    setFetching(true);
+    fetch("https://dummyjson.com/posts", signal)
       .then((res) => res.json())
       .then((data) => {
         addIntialPosts(data.posts);
+        setFetching(false);
       });
-    setDataFetched(true);
-  }
+
+    return () => {
+      console.log(controller);
+      controller.abort();
+    };
+  }, []);
+
   return (
     <div>
-      {postList.length === 0 && <WelcomeMessage />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </div>
   );
 };
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
-const WelcomeMessage = ({ onGetPostsClick }) => {
+const WelcomeMessage = () => {
   return (
     <center className="welcome-message">
       <h1>Welcome! No Posts Yet</h1>
-      <button
-        type="button"
-        onClick={onGetPostsClick}
-        className="btn btn-primary"
-      >
-        Get Post From Server
-      </button>
     </center>
   );
 };
